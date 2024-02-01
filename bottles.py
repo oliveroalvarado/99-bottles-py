d872d9ac895e68dba9e98aca8c88bf94c333543c
Oliver Alvarado <153557532+oliveroalvarado@users.noreply.github.com>
2024-02-01 09:01:49 -0800

    changes

--- bottles.py
+++ bottles.py
@@ -1,7 +1,20 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(bottles):
+	while bottles >= 0:
+		if bottles == 0:
+			print(f"""No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.""")
+		elif bottles == 1:
+			print(f"""{bottles} bottle of beer on the wall, {bottles} bottle of beer.
+Take one down and pass it around {bottles - 1} bottle of beer on the wall.""")
+		else:
+			print(f"""{bottles} bottles of beer on the wall, {bottles} bottles of beer.
+Take one down and pass it around {bottles - 1} bottles of beer on the wall.""")
+		bottles = bottles - 1
 
-bottle_song()
+	# pass
 
-####kldjsfalkfdajalskdfjdfls
+bottle_song(10)
+
+# ####kldjsfalkfdajalskdfjdfls
+
+
